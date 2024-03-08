f816e2d0bfa5a2d9d4a72aee87ed892e428e2dd6
Stas-Cell-Max <stanislav.morozan@gmail.com>
2024-03-08 18:45:47 -0500

    Removed the integrity attribute from  the Bootstrap and Font Awesome <link> tags

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -4,9 +4,9 @@ import React from 'react';
 
 const Footer = () => {
   return (
-    <footer className="footer bg-dark text-white mt-auto py-3"> {/* Set background color and text color */}
+    <footer className="footer bg-dark text-white mt-auto py-3"> 
       <div className="container">
-        <div className="row justify-content-center"> {/* Use Bootstrap's grid system for alignment */}
+        <div className="row justify-content-center"> 
           <div className="col-auto">
             <a href="https://github.com/username" className="text-white" target="_blank" rel="noopener noreferrer">
               <i className="fab fa-github"></i>
