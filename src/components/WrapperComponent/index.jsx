1e2cdc9fd1cbbe06def7443a0e6e54b4cbcba417
Stas-Cell-Max <stanislav.morozan@gmail.com>
2024-03-09 12:30:19 -0500

    Integrating all the needed components in Wrapper Component

--- src/components/WrapperComponent/index.jsx
+++ src/components/WrapperComponent/index.jsx
@@ -1,6 +1,11 @@
 import { Element } from "react-scroll";
 import AboutMe from "../AboutMe";
 import Home from "../Home";
+import WhatIDo from "../WhatIDo";
+import Resume from "../Resume";
+import Portfolio from "../Portfolio";
+import Testimonial from "../Testimonial"; // Assuming you have this component created
+import Contact from "../Contact";
 import "./index.css";
 
 const WrapperComponent = () => {
@@ -12,6 +17,21 @@ const WrapperComponent = () => {
       </Element>
       <Element className="w-full" name="aboutme">
         <AboutMe />
+      </Element>
+      <Element className="w-full" name="whatido">
+        <WhatIDo />
+      </Element>
+      <Element className="w-full" name="resume">
+        <Resume />
+      </Element>
+      <Element className="w-full" name="portfolio">
+        <Portfolio />
+      </Element>
+      <Element className="w-full" name="testimonial">
+        <Testimonial />
+      </Element>
+      <Element className="w-full" name="contact">
+        <Contact />
       </Element>
     </div>
   );
