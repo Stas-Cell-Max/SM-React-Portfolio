14198aa04306f292d15b68c6b2c88e9c25e6c0c4
Stas-Cell-Max <stanislav.morozan@gmail.com>
2024-03-10 12:45:08 -0400

    Added a backround image on WrapperComponent, updated its css and Home.css

--- src/components/Home/index.jsx
+++ src/components/Home/index.jsx
@@ -1,28 +1,19 @@
 // src/components/Home/Home.jsx
 
 import React from 'react';
-
 import './Home.css';
 // import backgroundImage from './profile.jpg'; // Make sure to have a background image in the assets folder
 
 const Home = () => {
+ 
   return (
-    <section id="home" style={{ 
-      // backgroundImage: `url(${backgroundImage})`,
-      backgroundSize: 'cover',
-      backgroundPosition: 'center',
-      height: '100vh',
-      display: 'flex',
-      flexDirection: 'column',
-      justifyContent: 'center',
-      alignItems: 'center',
-      color: 'white'
-    }}>
-      <h1 className="section-heading">Welcome</h1>
-        <hr className="my-4" />
-      <h2>I'm Stanislav Morozan</h2>
-      <p>Based in Toronto, Canada.</p>
-      <button className="btn btn-primary mt-4">Let's Talk</button>
+    <section id="home" className="home-section">
+     
+        <h1 className="section-heading">Welcome</h1>
+        <h2>I'm Stanislav Morozan</h2>
+        <p>Based in Toronto, Canada.</p>
+        <button className="btn btn-primary mt-4">Let's Talk</button>
+      
     </section>
   );
 };
