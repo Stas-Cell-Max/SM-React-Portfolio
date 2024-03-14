49fe7a4f82916392d3be9620b8e9c90b44fe9afd
Stas-Cell-Max <stanislav.morozan@gmail.com>
2024-03-14 18:20:52 -0400

    Added styling for ProjectDetailsModal component

--- src/components/Portfolio/index.jsx
+++ src/components/Portfolio/index.jsx
@@ -7,79 +7,21 @@ import './Portfolio.css';
 
 const Portfolio = () => {
 
-  // Define filter categories
-  const filters = {
-    ALL: '*',
-    WEB_DEV: 'web-dev',
-    ARCH_TECH: 'arch-tech'
-  };
-
-  const [filterKey, setFilterKey] = useState(filters.ALL);
-  const [selectedProject, setSelectedProject] = useState(null);
   const isotope = useRef();
+  // store the filter keyword in a state
+  const [filterKey, setFilterKey] = useState("*");
+  const [imagesLoaded, setimagesLoaded] = useState(0);
+  const [selectedProjectDetails, setSelectedProjectDetails] = useState();
 
-   // Initialize Isotope on component mount
-   useEffect(() => {
-    if (isotope.current) {
-      filterKey === filters.ALL
-        ? isotope.current.arrange({ filter: '*' })
-        : isotope.current.arrange({ filter: `.${filterKey}` });
-    }
-  }, [filterKey]);
-    
-  
-
-    // Update Isotope layout on filter change
-    useEffect(() => {
-      if (filterKey === '*') {
-        isotope.current.arrange({ filter: `*` });
-      } else {
-        isotope.current.arrange({ filter: `.${filterKey}` });
-      }
-    }, [filterKey]);
-
-    // Filter change handler
-  const handleFilterChange = (key) => {
-    setFilterKey(filters[key]);
+  const filters = {
+    DESIGN: "Desing",
+    BRAND: "Brand",
+    PHOTOS: "Photos",
   };
 
-   // Handle project selection
-   const handleProjectClick = (project) => {
-    setSelectedProject(project);
-  };
-    // Assuming I have a method to show the modal
-    // I may need to adapt this if I am using a different approach for modals
-  
-  const portfolioData = [
+  const projectsData = [
     {
-      title: "Las Vegas Trip Adviser",
-      projectInfo:
-        "Quidam lisque persius interesset his et, in quot quidam persequeris vim, ad mea essent possim iriure. Lisque persius interesset his et, in quot quidam persequeris vim, ad mea essent possim iriure.",
-      client: "Ruby Clinton",
-      technologies: "HTML, CSS3, JavaScript",
-      industry: "Tourism",
-      date: "July 16, 2023",
-      url: {
-        name: "www.example.com",
-        link: "https://www.example.com",
-      },
-      socialLinks: {
-        facebook: "http://www.facebook.com/",
-        twitter: "http://www.twitter.com/",
-        google: "http://www.google.com/",
-        instagram: "http://www.instagram.com/",
-        mail: "mailto:example@gmail.com",
-      },
-      thumbImage: "/images/project-.jpg",
-      sliderImages: [
-        "/images/project-2.jpeg" ,
-        "/images/project-2.jpeg" ,
-      ],
-      categories: [filters.BRAND],
-    },
-
-    {
-      title: "Project Title 6",
+      title: "Project Title 1",
       projectInfo:
         "Quidam lisque persius interesset his et, in quot quidam persequeris vim, ad mea essent possim iriure. Lisque persius interesset his et, in quot quidam persequeris vim, ad mea essent possim iriure.",
       client: "Ruby Clinton",
@@ -97,89 +39,10 @@ const Portfolio = () => {
         instagram: "http://www.instagram.com/",
         mail: "mailto:example@gmail.com",
       },
-      thumbImage: "/images/project-.jpg",
+      thumbImage: "images/projects/project-1.jpg",
       sliderImages: [
-        "/images/project-2.jpeg" ,
-        "/images/project-2.jpeg" ,
-      ],
-      categories: [filters.BRAND],
-    },
-
-    {
-      title: "Project Title 5",
-      projectInfo:
-        "Quidam lisque persius interesset his et, in quot quidam persequeris vim, ad mea essent possim iriure. Lisque persius interesset his et, in quot quidam persequeris vim, ad mea essent possim iriure.",
-      client: "Ruby Clinton",
-      technologies: "iOS, HTML5, CSS3, PHP, Java",
-      industry: "Art & Design",
-      date: "July 16, 2019",
-      url: {
-        name: "www.example.com",
-        link: "https://www.example.com",
-      },
-      socialLinks: {
-        facebook: "http://www.facebook.com/",
-        twitter: "http://www.twitter.com/",
-        google: "http://www.google.com/",
-        instagram: "http://www.instagram.com/",
-        mail: "mailto:example@gmail.com",
-      },
-      thumbImage: "/images/project-.jpg",
-      sliderImages: [
-        "/images/project-2.jpeg" ,
-        "/images/project-2.jpeg" ,
-      ],
-      categories: [filters.BRAND],
-    },
-    {
-      title: "Project Title 4",
-      projectInfo:
-        "Quidam lisque persius interesset his et, in quot quidam persequeris vim, ad mea essent possim iriure. Lisque persius interesset his et, in quot quidam persequeris vim, ad mea essent possim iriure.",
-      client: "Ruby Clinton",
-      technologies: "iOS, HTML5, CSS3, PHP, Java",
-      industry: "Art & Design",
-      date: "July 16, 2019",
-      url: {
-        name: "www.example.com",
-        link: "https://www.example.com",
-      },
-      socialLinks: {
-        facebook: "http://www.facebook.com/",
-        twitter: "http://www.twitter.com/",
-        google: "http://www.google.com/",
-        instagram: "http://www.instagram.com/",
-        mail: "mailto:example@gmail.com",
-      },
-      thumbImage: "/images/project-.jpg",
-      sliderImages: [
-        "/images/project-2.jpeg" ,
-        "/images/project-2.jpeg" ,
-      ],
-      categories: [filters.BRAND],
-    },
-    {
-      title: "Project Title 3",
-      projectInfo:
-        "Quidam lisque persius interesset his et, in quot quidam persequeris vim, ad mea essent possim iriure. Lisque persius interesset his et, in quot quidam persequeris vim, ad mea essent possim iriure.",
-      client: "Ruby Clinton",
-      technologies: "iOS, HTML5, CSS3, PHP, Java",
-      industry: "Art & Design",
-      date: "July 16, 2019",
-      url: {
-        name: "www.example.com",
-        link: "https://www.example.com",
-      },
-      socialLinks: {
-        facebook: "http://www.facebook.com/",
-        twitter: "http://www.twitter.com/",
-        google: "http://www.google.com/",
-        instagram: "http://www.instagram.com/",
-        mail: "mailto:example@gmail.com",
-      },
-      thumbImage: "/images/project-.jpg",
-      sliderImages: [
-        "/images/project-2.jpeg" ,
-        "/images/project-2.jpeg" ,
+        "images/projects/project-1.jpg",
+        "images/projects/project-5.jpg",
       ],
       categories: [filters.BRAND],
     },
@@ -202,42 +65,269 @@ const Portfolio = () => {
         instagram: "http://www.instagram.com/",
         mail: "mailto:example@gmail.com",
       },
-      thumbImage: "/images/project-.jpg",
+      thumbImage: "images/projects/project-2.jpg",
       sliderImages: [
-        "/images/project-2.jpeg" ,
-        "/images/project-2.jpeg" ,
+        "images/projects/project-2.jpg",
+        "images/projects/project-5.jpg",
+      ],
+      categories: [filters.DESIGN],
+    },
+    {
+      title: "Project Title 3",
+      projectInfo:
+        "Quidam lisque persius interesset his et, in quot quidam persequeris vim, ad mea essent possim iriure. Lisque persius interesset his et, in quot quidam persequeris vim, ad mea essent possim iriure.",
+      client: "Ruby Clinton",
+      technologies: "iOS, HTML5, CSS3, PHP, Java",
+      industry: "Art & Design",
+      date: "July 16, 2019",
+      url: {
+        name: "www.example.com",
+        link: "https://www.example.com",
+      },
+      socialLinks: {
+        facebook: "http://www.facebook.com/",
+        twitter: "http://www.twitter.com/",
+        google: "http://www.google.com/",
+        instagram: "http://www.instagram.com/",
+        mail: "mailto:example@gmail.com",
+      },
+      thumbImage: "images/projects/project-3.jpg",
+      sliderImages: [
+        "images/projects/project-3.jpg",
+        "images/projects/project-5.jpg",
+      ],
+      categories: [filters.PHOTOS],
+    },
+    {
+      title: "Project Title 4",
+      projectInfo:
+        "Quidam lisque persius interesset his et, in quot quidam persequeris vim, ad mea essent possim iriure. Lisque persius interesset his et, in quot quidam persequeris vim, ad mea essent possim iriure.",
+      client: "Ruby Clinton",
+      technologies: "iOS, HTML5, CSS3, PHP, Java",
+      industry: "Art & Design",
+      date: "July 16, 2019",
+      url: {
+        name: "www.example.com",
+        link: "https://www.example.com",
+      },
+      socialLinks: {
+        facebook: "http://www.facebook.com/",
+        twitter: "http://www.twitter.com/",
+        google: "http://www.google.com/",
+        instagram: "http://www.instagram.com/",
+        mail: "mailto:example@gmail.com",
+      },
+      thumbImage: "images/projects/project-4.jpg",
+      sliderImages: [
+        "images/projects/project-1.jpg",
+        "images/projects/project-4.jpg",
+      ],
+      categories: [filters.BRAND, filters.PHOTOS],
+    },
+    {
+      title: "Project Title 5",
+      projectInfo:
+        "Quidam lisque persius interesset his et, in quot quidam persequeris vim, ad mea essent possim iriure. Lisque persius interesset his et, in quot quidam persequeris vim, ad mea essent possim iriure.",
+      client: "Ruby Clinton",
+      technologies: "iOS, HTML5, CSS3, PHP, Java",
+      industry: "Art & Design",
+      date: "July 16, 2019",
+      url: {
+        name: "www.example.com",
+        link: "https://www.example.com",
+      },
+      socialLinks: {
+        facebook: "http://www.facebook.com/",
+        twitter: "http://www.twitter.com/",
+        google: "http://www.google.com/",
+        instagram: "http://www.instagram.com/",
+        mail: "mailto:example@gmail.com",
+      },
+      thumbImage: "images/projects/project-5.jpg",
+      sliderImages: [
+        "images/projects/project-1.jpg",
+        "images/projects/project-5.jpg",
+      ],
+      categories: [filters.DESIGN],
+    },
+    {
+      title: "Project Title 6",
+      projectInfo:
+        "Quidam lisque persius interesset his et, in quot quidam persequeris vim, ad mea essent possim iriure. Lisque persius interesset his et, in quot quidam persequeris vim, ad mea essent possim iriure.",
+      client: "Ruby Clinton",
+      technologies: "iOS, HTML5, CSS3, PHP, Java",
+      industry: "Art & Design",
+      date: "July 16, 2019",
+      url: {
+        name: "www.example.com",
+        link: "https://www.example.com",
+      },
+      socialLinks: {
+        facebook: "http://www.facebook.com/",
+        twitter: "http://www.twitter.com/",
+        google: "http://www.google.com/",
+        instagram: "http://www.instagram.com/",
+        mail: "mailto:example@gmail.com",
+      },
+      thumbImage: "images/projects/project-6.jpg",
+      sliderImages: [
+        "images/projects/project-1.jpg",
+        "images/projects/project-5.jpg",
       ],
       categories: [filters.BRAND],
     },
+    {
+      title: "Project Title 7",
+      projectInfo:
+        "Quidam lisque persius interesset his et, in quot quidam persequeris vim, ad mea essent possim iriure. Lisque persius interesset his et, in quot quidam persequeris vim, ad mea essent possim iriure.",
+      client: "Ruby Clinton",
+      technologies: "iOS, HTML5, CSS3, PHP, Java",
+      industry: "Art & Design",
+      date: "July 16, 2019",
+      url: {
+        name: "www.example.com",
+        link: "https://www.example.com",
+      },
+      socialLinks: {
+        facebook: "http://www.facebook.com/",
+        twitter: "http://www.twitter.com/",
+        google: "http://www.google.com/",
+        instagram: "http://www.instagram.com/",
+        mail: "mailto:example@gmail.com",
+      },
+      thumbImage: "images/projects/project-7.jpg",
+      sliderImages: [
+        "images/projects/project-1.jpg",
+        "images/projects/project-5.jpg",
+      ],
+      categories: [filters.DESIGN, filters.PHOTOS],
+    },
   ];
+
+  // initialize an Isotope object with configs
+  useEffect(() => {
+    isotope.current = new Isotope(".portfolio-filter", {
+      itemSelector: ".filter-item",
+      layoutMode: "masonry",
+    });
+
+    // cleanup
+    return () => {
+      isotope.current.destroy();
+    };
+  }, []);
+
+  // handling filter key change
+  useEffect(() => {
+    if (imagesLoaded) {
+      filterKey === "*"
+        ? isotope.current.arrange({ filter: '.Design' })
+        : isotope.current.arrange({ filter: `.${filterKey}` });
+    }
+  }, [filterKey, imagesLoaded]);
+
+  const handleFilterKeyChange = (key) => () => setFilterKey(key);
 
   return (
     <>
-      <section id="portfolio" className="portfolio-section">
-        {/* Buttons for filtering */}
-        <div className="portfolio-filters">
-          <button onClick={() => handleFilterChange('filters.ALL')}>All</button>
-          <button onClick={() => handleFilterChange('filters.WEB_DEV')}>Web Development</button>
-          <button onClick={() => handleFilterChange('filters.ARCH_TECH')}>Architect Technologist</button>
-        </div>
-
-        <div className="grid">
-          {/* Map through the projects and display them */}
-          {portfolioData.map(project => (
-            <div key={project.id} className={`grid-item ${project.categories.join(' ')}`}>
-              <img src={project.thumbImage} alt={project.title} />
-              <button onClick={() => handleProjectClick(project)}>View Details</button>
+      <section
+        id="portfolio"
+        className="container px-lg-5"
+      >
+        <div className="container px-lg-5">
+          {/* Heading */}
+          <div className="position-relative d-flex text-center mb-5">
+            <h2
+              className="text-24 text-uppercase fw-600 w-100 mb-0 text-light opacity-4"
+            >
+              Portfolio
+              </h2>
+          <p className="text-9 text-dark fw-600 position-absolute w-100 align-self-center lh-base mb-0">
+            My Work
+            <span className="heading-separator-line border-bottom border-3 border-primary d-block mx-auto" />
+          </p>
+          </div>
+          {/* Heading end*/}
+          {/* Filter Menu */}
+          <ul
+            className="position-relative d-flex text-center mb-5"
+          >
+            <li className="nav-item">
+              <button
+                className={"nav-link " + (filterKey === "*" ? "active" : "")}
+                onClick={handleFilterKeyChange("*")}
+              >
+                All
+              </button>
+            </li>
+            {Object.keys(filters).map((oneKey, i) => (
+              <li className="nav-item" key={i}>
+                <button
+                  className={
+                    "nav-link " +
+                    (filterKey === filters[oneKey] ? "active" : "")
+                  }
+                  onClick={handleFilterKeyChange(filters[oneKey])}
+                >
+                  {filters[oneKey]}
+                </button>
+              </li>
+            ))}
+          </ul>
+          {/* Filter Menu end */}
+          <div className="portfolio popup-ajax-gallery">
+            <div className="row portfolio-filter filter-container g-4">
+              {projectsData.length > 0 &&
+                projectsData.map((project, index) => (
+                  <div
+                    className={
+                      "col-sm-6 col-lg-4 filter-item " +
+                      project.categories.join(" ")
+                    }
+                    key={index}
+                  >
+                    <div className="portfolio-box rounded">
+                      <div className="portfolio-img rounded">
+                        <img
+                          onLoad={() => {
+                            setimagesLoaded(imagesLoaded + 1);
+                          }}
+                          className="img-fluid d-block portfolio-image"
+                          src={project.thumbImage}
+                          alt=""
+                        />
+                        <div className="portfolio-overlay">
+                          <a
+                            className="popup-ajax stretched-link"
+                            href=""
+                            onClick={() => {
+                              setSelectedProjectDetails(projectsData[index]);
+                            }}
+                            data-bs-toggle="modal"
+                            data-bs-target="#exampleModal"
+                          />
+                          <div className="portfolio-overlay-details">
+                            <h5 className="text-white fw-400">
+                              {project.title}
+                            </h5>
+                            <span className="text-light">Category</span>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                ))}
             </div>
-          ))}
+          </div>
         </div>
       </section>
-      
-      {selectedProject && (
-  <ProjectDetailsModal
-    project={selectedProject}
-    closeModal={() => setSelectedProject(null)}
-        />
-      )}
+      <div className="project-details-modal">
+        {/* Modal */}
+        <ProjectDetailsModal
+          projectDetails={selectedProjectDetails}
+          darkTheme={false}
+        ></ProjectDetailsModal>
+      </div>
     </>
   );
 };
