7ea366d34cc3778f500c4b496f61c397841752ce
Stas-Cell-Max <stanislav.morozan@gmail.com>
2024-03-21 10:07:29 -0400

    Update Vite configuration for GitHub Pages deployment

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,10 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
+  base: '/SM-React-Portfolio/',
   plugins: [react()],
-})
+  build: {
+    outDir: 'build', // If you changed this according to previous steps, keep it. Otherwise, it defaults to 'dist'
+  },
+});
