c0a2b203381f4e5e783197cc5f9eabcf64840860
Stas-Cell-Max <stanislav.morozan@gmail.com>
2024-04-03 22:43:00 -0400

    Imported the HamburgerHeader component in App.jsx, the windowWidth state is initialized

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@
 import React from "react";
 import { BrowserRouter as Router } from "react-router-dom";
 import Header from "./components/Header";
+import HamburgerHeader from "./components/HamburgerHeader";
 import WrapperComponent from "./components/WrapperComponent";
 import "slick-carousel/slick/slick.css";         
 import "slick-carousel/slick/slick-theme.css";
@@ -10,6 +11,14 @@ import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 import './App.css';
 
 function App() {
+
+  const [windowWidth, setWindowWidth] = useState(window.innerWidth);
+
+  useEffect(() => {
+    const handleResize = () => setWindowWidth(window.innerWidth);
+    window.addEventListener('resize', handleResize);
+    return () => window.removeEventListener('resize', handleResize);
+  }, []);
 
       // Define the function to handle navigation click
       const handleNavClick = (elementId) => {
@@ -22,9 +31,10 @@ function App() {
   return (
   
     <Router>
-      <Header handleNavClick={handleNavClick}/>
-      <WrapperComponent handleNavClick={handleNavClick}/>
-    </Router>
+    {/* Conditionally render Header or HamburgerHeader based on windowWidth */}
+    {windowWidth > 768 ? <Header handleNavClick={handleNavClick}/> : <HamburgerHeader handleNavClick={handleNavClick}/>}
+    <WrapperComponent handleNavClick={handleNavClick}/>
+  </Router>
   );
 }
 
