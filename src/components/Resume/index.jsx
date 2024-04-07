3014a714f66556d1f6bfeb014274f891a909a6ae
Stas-Cell-Max <stanislav.morozan@gmail.com>
2024-04-06 23:15:52 -0400

    Update on WhatIDo

--- src/components/Resume/index.jsx
+++ src/components/Resume/index.jsx
@@ -8,13 +8,13 @@ const Resume = () => {
     const educationDetails = [
       {
         yearRange: "2005 - 2010",
-        title: "Technical University of Moldova",
-        place: "Information Technology",
+        title: "Science of Information Technology",
+        place: "Technical University of Moldova",
         desc: "Coursework encompassed network architecture, database management, software development, web technologies, cybersecurity, system analysis, and IT project management, equipping me with a foundational understanding of technology in business operations.",
       },
       {
         yearRange: "2014 - 2018",
-        title: "Archietctural Technology",
+        title: "Architectural Technology",
         place: "George Brown College",
         desc: "Comprehensive study in building design and architectural drafting, BIM (Building Information Modeling), sustainable design principles, construction technology, and project management, preparing for innovative solutions in architectural development and execution.",
       },
@@ -30,7 +30,7 @@ const Resume = () => {
         yearRange: "2016 - 2018",
         title: "Architectural Technologist",
         place: "NORR",
-        desc: "Enabled digital transformation of architectural solutions, translating physical design principles into the virtual realm, which enhanced client presentations and project bids.  Facilitated the high-fidelity digital conversion of technical drawings and specifications, ensuring detailed accuracy and improving project team collaboration by 20%.  Led the adoption of data-driven design techniques, leveraging analytics to inform design decisions and enhance UI adaptability, significantly reducing revision cycles by 25%. ",
+        desc: "Enabled digital transformation of architectural solutions, translating physical design principles into the virtual realm, which enhanced client presentations and project bids. Facilitated the high-fidelity digital conversion of technical drawings and specifications, ensuring detailed accuracy and improving project team collaboration by 20%.  Led the adoption of data-driven design techniques, leveraging analytics to inform design decisions and enhance UI adaptability, significantly reducing revision cycles by 25%. ",
       },
       {
         yearRange: "2018 - 2020",
@@ -42,37 +42,9 @@ const Resume = () => {
         yearRange: "2020 - 2024",
         title: "Tech Integration Architect",
         place: "One York Construction Management",
-        desc: "Revolutionized client engagement by architecting the company's website from the ground up,leveraging architectural design principles to enhance visualization and interactivity. Direct involvement led to a 20% increase in online project inquiries.  Orchestrated the end-to-end development of a construction project management platform, facilitating unprecedented collaboration among stakeholders. This initiative reduced project delivery times by 25% and significantly improved stakeholder satisfaction.  Championed user-centric design, establishing robust feedback loops that informed continuous improvements, resulting in a 40% uptick in platform user satisfaction. Led targeted training programs that bolstered platform adoption and user proficiency. ",
+        desc: "Revolutionized client engagement by architecting the company's website from the ground up,leveraging architectural design principles to enhance visualization and interactivity. Direct involvement led to a 20% increase in online project inquiries. Orchestrated the end-to-end development of a construction project management platform, facilitating unprecedented collaboration among stakeholders. This initiative reduced project delivery times by 25% and significantly improved stakeholder satisfaction.  Championed user-centric design, establishing robust feedback loops that informed continuous improvements, resulting in a 40% uptick in platform user satisfaction. Led targeted training programs that bolstered platform adoption and user proficiency. ",
       },
     ];
-
-    const skills = [
-      {
-        name: "Node.js",
-        percent: 65,
-      },
-      {
-        name: "HTML/CSS",
-        percent: 95,
-      },
-      {
-        name: "Java Script",
-        percent: 80,
-      },
-      {
-        name: "React JS",
-        percent: 70,
-      },
-      {
-        name: "MongoDB",
-        percent: 60,
-      },
-      {
-        name: "Bootstrap",
-        percent: 75,
-      },
-    ];
-        
 
     return (
       <section
@@ -105,13 +77,13 @@ const Resume = () => {
           <div className="row gx-5">
             {/* My Education */}
             <div className="col-md-6 col-sm-12">
-              <h2
+              <h3
                 className={
                   "text-6 fw-600 mb-4 " 
                 }
               >
                 My Education
-              </h2>
+              </h3>
               {educationDetails.length > 0 &&
                 educationDetails.map((value, index) => (
                   <div
@@ -137,13 +109,13 @@ const Resume = () => {
             </div>
             {/* My Experience */}
             <div className="col-md-6 col-sm-12">
-              <h2
+              <h3
                 className={
                   "text-6 fw-600 mb-4 " 
                 }
               >
                 My Experience
-              </h2>
+              </h3>
               {experienceDetails.length > 0 &&
                 experienceDetails.map((value, index) => (
                   <div
