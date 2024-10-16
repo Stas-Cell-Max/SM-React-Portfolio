ce0211b1be711466719e65fbcc82dbcae91b6595
Stas-Cell-Max <stanislav.morozan@gmail.com>
2024-10-15 23:10:11 -0400

    renamed the page

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: '/SM-React-Portfolio/',
+  base: '/StanislavMorozan-Portfolio/',
   plugins: [react()],
   build: {
     outDir: 'build', // If you changed this according to previous steps, keep it. Otherwise, it defaults to 'dist'
